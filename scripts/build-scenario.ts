35e6050aebace3fb8d5de491020fc3f5061f931e
Mattias Nyberg <muthaias.uu@gmail.com>
2020-10-18 15:26:26 +0200

    feat: add content scripts using sub module

---
+++ scripts/build-scenario.ts
@@ -0,0 +1,63 @@
+import { resolve, join } from 'path'
+import { promises } from 'fs'
+const { writeFile, mkdir } = promises
+
+import {
+    GameWorld,
+    ScenarioBuilder,
+    Scenario
+} from './content-utils'
+
+async function buildScenario(id: string) {
+    try {
+        const builder: ScenarioBuilder = (
+            await import(`./scenarios/${id}/index.ts`)
+        ).builder
+
+        const scenario = builder.run()
+        console.log(`✅ Sucessfully built scenario "${id}"`)
+
+        return scenario
+    } catch (e) {
+        throw new Error(`Error: Failed to build scenario "${id}" - ${e}`)
+    }
+}
+
+async function exportScenario(outputDir: string, scenario: Scenario) {
+    type Parts = {
+        [K in keyof GameWorld]: string
+    }
+
+    const scenarioParts: Parts = {
+        cards: 'cards',
+        eventCards: 'event-cards',
+        events: 'events',
+        defaultState: 'default-state',
+        stats: 'stats',
+        worldStateModifiers: 'modifiers',
+    }
+
+    await mkdir(outputDir, { recursive: true })
+
+    await Promise.all(
+        Object.entries(scenarioParts).map(([part, fileName]) =>
+            writeFile(
+                resolve(join(outputDir, `${fileName}.json`)),
+                JSON.stringify(scenario[part as keyof GameWorld], null, 4),
+            ),
+        ),
+    )
+
+    console.log(`✅ Sucessfully exported scenario "${scenario.id}" to ${outputDir}`)
+}
+
+if (require.main === module) {
+    const id = process.argv.length >= 3 ? process.argv[2] : 'default'
+    const basePath = process.argv.length >= 4 ? process.argv[3] : 'dist'
+    
+    buildScenario(id).then((scenario: Scenario) => {
+        const path = resolve(join(basePath, id))
+        exportScenario(path, scenario)
+    })
+}
+  
---
+++ scripts/scenarios/default/cards.ts
@@ -0,0 +1,84 @@
+import {
+    CardData,
+    worldQuery,
+    addAction,
+    unsplashImage,
+    pexelsImage,
+} from '../../content-utils'
+import { ENVIRONMENT, MONEY, PEOPLE, SECURITY, POPULARITY } from './stats'
+import { VARS } from './vars'
+import { FLAGS } from './flags'
+
+export const otherCards: CardData[] = [
+    {
+        type: 'card',
+        image: unsplashImage('1497435334941-8c899ee9e8e9'),
+        title: 'Our solar project is ready!',
+        location: 'The greener other side',
+        text:
+            'Congratulations! We beat the initial German energy expansion ⚡️',
+        weight: 100,
+        isAvailableWhen: [
+            worldQuery(
+                {
+                    [VARS.SOLAR_INVESTMENTS]: [1, 1],
+                },
+                {
+                    [FLAGS.INFRAN_INIT]: true,
+                },
+            ),
+        ],
+        actions: {
+            left: addAction({
+                [ENVIRONMENT]: 30,
+                [PEOPLE]: 15,
+                [SECURITY]: 15,
+                [MONEY]: 5,
+                [POPULARITY]: 20,
+                [VARS.SOLAR_INVESTMENTS]: 100,
+            }),
+            right: addAction({
+                [ENVIRONMENT]: 30,
+                [PEOPLE]: 15,
+                [SECURITY]: 15,
+                [MONEY]: 5,
+                [POPULARITY]: 20,
+                [VARS.SOLAR_INVESTMENTS]: 100,
+            }),
+        },
+    },
+    {
+        type: 'card',
+        image: pexelsImage('3044473'),
+        title: 'Cheap but dirty brown coal for sale',
+        location: 'Working class district',
+        text:
+            "We've got an interesting offer: Buy a modern brown coal power plant cheaply to generate electricity. Deal? Great! (WATCH OUT FOR ENVIRA though...)",
+        weight: 100,
+        isAvailableWhen: [
+            worldQuery({
+                [ENVIRONMENT]: [21, 100],
+                [MONEY]: [15, 100],
+                [VARS.BROWN_COAL_PLANTS]: [0, 0],
+            }),
+        ],
+        actions: {
+            left: addAction({
+                [ENVIRONMENT]: 10,
+                [PEOPLE]: 10,
+                [SECURITY]: 15,
+                [MONEY]: -5,
+                [POPULARITY]: 25,
+                [VARS.BROWN_COAL_PLANTS]: 0,
+            }),
+            right: addAction({
+                [ENVIRONMENT]: -20,
+                [PEOPLE]: -15,
+                [SECURITY]: -10,
+                [MONEY]: 40,
+                [POPULARITY]: -20,
+                [VARS.BROWN_COAL_PLANTS]: 1,
+            }),
+        },
+    },
+]
