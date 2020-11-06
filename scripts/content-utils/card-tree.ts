d50a9f528010c640bc5b1107ae74714a805fb8b1
Samuel Plumppu <greenheartmusic@gmail.com>
2020-11-06 20:38:26 +0100

    feature(CardTree): Add initial support for defining connected cards using a tree structure
    Co-authored-by: Mattias Nyberg <muthaias.uu@gmail.com>

---
+++ scripts/content-utils/card-tree.ts
@@ -0,0 +1,70 @@
+import {
+    BaseCard,
+    WorldQuery,
+    CardData,
+    GameWorldModifier,
+    cardRef,
+    cardLogic,
+    setAction,
+    combineWorldQueries,
+} from "./"
+
+export type CardTree = {
+    card: BaseCard
+    left?: CardTree
+    right?: CardTree
+    conditions?: WorldQuery[]
+    action?: GameWorldModifier
+}
+
+export function cardsFromTree(tree: CardTree, bindRef?: string): CardData[] {
+    const leftRef = cardRef(tree.card.title + " left")
+    const rightRef = cardRef(tree.card.title + " right")
+    const triggerRef = bindRef
+        ? undefined
+        : cardRef(tree.card.title + " origin")
+    const triggerRefRemoval = triggerRef ? { [triggerRef]: true } : {}
+    const bindRefRemoval = bindRef ? { [bindRef]: false } : {}
+    const conditions = tree.conditions ? tree.conditions : [{}]
+    return [
+        cardLogic(
+            tree.card,
+            conditions.map((c) =>
+                combineWorldQueries(c, {
+                    flags: {
+                        ...(bindRef
+                            ? {
+                                  [bindRef]: true,
+                              }
+                            : {}),
+                        ...(triggerRef
+                            ? {
+                                  [triggerRef]: false,
+                              }
+                            : {}),
+                    },
+                }),
+            ),
+            [
+                setAction(
+                    {},
+                    {
+                        [leftRef]: true,
+                        ...bindRefRemoval,
+                        ...triggerRefRemoval,
+                    },
+                ),
+                setAction(
+                    {},
+                    {
+                        [rightRef]: true,
+                        ...bindRefRemoval,
+                        ...triggerRefRemoval,
+                    },
+                ),
+            ],
+        ),
+        ...(tree.left ? cardsFromTree(tree.left, leftRef) : []),
+        ...(tree.right ? cardsFromTree(tree.right, rightRef) : []),
+    ]
+}
