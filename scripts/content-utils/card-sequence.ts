94d96ada9612fa1ae4a3ced4f005ad2c92f7f109
Samuel Plumppu <greenheartmusic@gmail.com>
2020-11-14 21:16:36 +0100

    fix(cardsFromSequence): Cleanup and add ideas for solving remaining bugs

--- scripts/content-utils/card-sequence.ts
+++ scripts/content-utils/card-sequence.ts
@@ -9,9 +9,9 @@ import { cardsFromTree } from "./card-tree"
  * - fix bug to only show final card in sequence once
  * - (maybe) fix removing unused flags after they have been used to trigger cards in the sequence
  *
- * 2) Add support to execute different modifiers on left and right
+ * 2) ✅ Add support to execute different modifiers on left and right (implemented by CardTree)
  *
- * 3) Add support for CardTree within the CardSequenuce. If a node is a CardTree, then recursively call the cardsFromTree() function to build that subtree.
+ * 3) ✅ Add support for CardTree within the CardSequenuce. If a node is a CardTree, then recursively call the cardsFromTree() function to build that subtree.
  * 4) Add support for CardSequence within the CardTree. If a node is a CardSequence, then then recursively call the cardsFromSequence() function to build that subsequence.
  */
 
@@ -29,30 +29,6 @@ export function cardsFromSequence(
         cardRef(tree.card.title + " sequence"),
     )
 
-    // TODO: remove unused params and simplify functions if possible.
-    function getStartConditions(prevRef: string, currentRef: string) {
-        return [
-            {
-                flags: {
-                    ...(prevRef ? { [prevRef]: true } : {}),
-                    ...(currentRef ? { [currentRef]: false } : {}),
-                },
-            },
-        ]
-    }
-
-    function getEndModifiers(currentRef: string, nextRef: string) {
-        return [
-            setModifier(
-                {},
-                {
-                    ...(currentRef ? { [currentRef]: true } : {}),
-                    // ...(nextRef ? { [nextRef]: true } : {}),
-                },
-            ),
-        ]
-    }
-
     const cards = sequence.flatMap((tree, i) => {
         const prevRef = sequenceRefs[i - 1]
         const currentRef = sequenceRefs[i]
@@ -68,7 +44,14 @@ export function cardsFromSequence(
                 ? _startConditions
                 : getStartConditions(prevRef, currentRef)
 
-        console.log(startConditions)
+        const moveToNextCardInSequence = getEndModifiers(currentRef, nextRef)
+
+        // TODO: Fix bug that the last card isn't removed after sequence is completed
+        // TODO: Fix so that _endModifiers to apply at the end of a CardTree within a sequence are remainig intact, even when used within a Sequence.
+
+        // if we need to support passing down endModifies and startConditions multiple levels into nested CardTrees and CardSequences,
+        // then one possible solution might be to store everything needed in a object, passed as the final argument, where each level can add its references, modifiers, conditions etc.
+        // Then as the levels are resolved, keys and values are removed from the object to only keep relevant data there during runtime.
 
         const endModifiers = nextRef
             ? getEndModifiers(currentRef, nextRef)
@@ -80,38 +63,28 @@ export function cardsFromSequence(
     })
 
     return cards
+}
 
-    /*
+// TODO: remove unused params and simplify functions if possible.
+function getStartConditions(prevRef: string, currentRef: string) {
+    return [
+        {
+            flags: {
+                ...(prevRef ? { [prevRef]: true } : {}),
+                ...(currentRef ? { [currentRef]: false } : {}),
+            },
+        },
+    ]
+}
 
-        _startConditions = [{}]
-        _endModifiers = [{}]
-    
-        for (node, index of sequence)
-        
-            if node is CardTree
-
-                if (index === 0)
-
-                cardsFromTree(node)
-    
-    */
-
-    // return sequence.map((cardTree) => {
-    //     // TODO: Maybe we need to set the start/end based on the prev/next card?
-    //     const startConditions = {}
-    //     const endModifiers = {}
-    //     return cardsFromTree(cardTree, startConditions, endModifiers)
-    // })
-
-    // map to create cardsFromTree(card, startConditions, endModifiers)
-
-    // - - - - X 1 2 3 -
-
-    /*
-        if CardTree is found in sequence
-            pass onFinishAction to cardsFromSequence() to automatically add this as the last action on CardLeafs
-            // this will ensure we know the onFinishAction in the outside context,
-            // allowing the next card in the sequence to show up when the CardTree has been completed.
-
-    */
+function getEndModifiers(currentRef: string, nextRef: string) {
+    return [
+        setModifier(
+            {},
+            {
+                ...(currentRef ? { [currentRef]: true } : {}),
+                // ...(nextRef ? { [nextRef]: true } : {}),
+            },
+        ),
+    ]
 }
