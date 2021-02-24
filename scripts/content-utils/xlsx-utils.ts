4cfd5336a13ac3e343bb3b79a19797b61a7457b5
Mattias Nyberg <muthaias.uu@gmail.com>
2021-02-24 23:03:26 +0100

    feat: add sheet filter option to xlsx import

--- scripts/content-utils/xlsx-utils.ts
+++ scripts/content-utils/xlsx-utils.ts
@@ -8,10 +8,17 @@ export type TypeMapping<T> = {
 export function loadFile<T>(
     path: string,
     fieldMapping: TypeMapping<T>,
+    {
+        sheetIds,
+    }: Partial<{
+        sheetIds: string[]
+    }> = {},
 ): (T & { __sheet?: string })[] {
     const book = xlsx.read(fs.readFileSync(path))
     const requiredFields: (keyof T)[] = Object.keys(fieldMapping) as (keyof T)[]
     const transformedSheets = Object.keys(book.Sheets).map<T[]>((key) => {
+        if (sheetIds !== undefined && !sheetIds.includes(key)) return []
+
         const sheet = book.Sheets[key]
         const data = xlsx.utils.sheet_to_json<{ [A in keyof T]: unknown }>(
             sheet,
--- scripts/scenarios/coal-mining/from-data.ts
+++ scripts/scenarios/coal-mining/from-data.ts
@@ -1,4 +1,4 @@
-import { loadFile } from "../../content-utils/xlsx-utils"
+import { loadFile, toString } from "../../content-utils/xlsx-utils"
 
 const rawData = loadFile<{
     "Card Name": string
@@ -8,13 +8,19 @@ const rawData = loadFile<{
     "Swipe Left Text": string
     "Swipe Right Text": string
     "Image Desc": string
-}>("./scripts/scenarios/coal-mining/data/base.xlsx", {
-    "Card Name": toString,
-    "Card Type": toString,
-    Character: toString,
-    "Text of card": toString,
-    "Swipe Left Text": toString,
-    "Swipe Right Text": toString,
-    "Image Desc": toString,
-})
+}>(
+    "./scripts/scenarios/coal-mining/data/base.xlsx",
+    {
+        "Card Name": toString,
+        "Card Type": toString,
+        Character: toString,
+        "Text of card": toString,
+        "Swipe Left Text": toString,
+        "Swipe Right Text": toString,
+        "Image Desc": toString,
+    },
+    {
+        sheetIds: ["Sequence 0"],
+    },
+)
 console.log(rawData)
