a28e1ff65572c85be02820d3d41f6b21cf5bde25
Mattias Nyberg <muthaias.uu@gmail.com>
2021-02-24 22:04:10 +0100

    feat: add initial xlsx loading support util

---
+++ scripts/content-utils/xlsx-utils.ts
@@ -0,0 +1,41 @@
+import xlsx from "xlsx"
+import fs from "fs"
+
+export type TypeMapping<T> = {
+    [A in keyof T]: (value: unknown) => T[A]
+}
+
+export function loadFile<T>(path: string, fieldMapping: TypeMapping<T>): T[] {
+    const book = xlsx.read(fs.readFileSync(path))
+    const requiredFields: (keyof T)[] = Object.keys(fieldMapping) as (keyof T)[]
+    const transformedSheets = Object.keys(book.Sheets).map<T[]>((key) => {
+        const sheet = book.Sheets[key]
+        const data = xlsx.utils.sheet_to_json<{ [A in keyof T]: unknown }>(
+            sheet,
+            {
+                header: 0,
+            },
+        )
+        return data.map(
+            (d) =>
+                requiredFields.reduce<Partial<T>>(
+                    (acc: Partial<T>, fieldId) => {
+                        return {
+                            ...acc,
+                            [fieldId]: fieldMapping[fieldId](d[fieldId]),
+                        }
+                    },
+                    {},
+                ) as T,
+        )
+    })
+    return new Array<T>().concat(...transformedSheets)
+}
+
+export function toString(value: any): string {
+    return value ? value + "" : ""
+}
+
+export function toNumber(value: any): number {
+    return value ? parseFloat(value) : 0
+}
---
+++ scripts/scenarios/coal-mining/from-data.ts
@@ -0,0 +1,20 @@
+import { loadFile } from "../../content-utils/xlsx-utils"
+
+const rawData = loadFile<{
+    "Card Name": string
+    "Card Type": string
+    Character: string
+    "Text of card": string
+    "Swipe Left Text": string
+    "Swipe Right Text": string
+    "Image Desc": string
+}>("/home/mattiasnyberg/Downloads/MVP Card List.xlsx", {
+    "Card Name": toString,
+    "Card Type": toString,
+    Character: toString,
+    "Text of card": toString,
+    "Swipe Left Text": toString,
+    "Swipe Right Text": toString,
+    "Image Desc": toString,
+})
+console.log(rawData)
