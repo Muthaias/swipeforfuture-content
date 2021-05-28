9c71313f16eafd0d7683bb14704e06c719835e4d
Mattias <muthaias.uu@gmail.com>
2021-05-28 23:09:27 +0200

    fix: add solar and wind farm to image dv

--- scripts/scenarios/coal-mining/images.ts
+++ scripts/scenarios/coal-mining/images.ts
@@ -25,10 +25,11 @@ export const { getImage } = imageDB([
     imageEntry(unsplashImage("1529088746738-c4c0a152fb2c"), "infran"),
     imageEntry(unsplashImage("1548247416-ec66f4900b2e"), "cat"),
     imageEntry(unsplashImage("1548247416-ec66f4900b2e"), "default"),
+    imageEntry(unsplashImage("1600187777399-220e82950adc"), "Wind Farm", "wind"),
     imageEntry(pexelsImage("3044473"), "powerplant", "coal"),
     imageEntry(
         unsplashImage("1497435334941-8c899ee9e8e9"),
-        "powerplant",
+        "Solar Farm",
         "solar",
     ),
 ])
